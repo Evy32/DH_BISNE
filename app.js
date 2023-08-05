9c309a4905949ff3624d49c3a5c38e92a7cbdaa5
IsaCP14 <isabel.carrero88@gmail.com>
2023-08-05 12:18:06 -0500

    middlewares

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-require('dotenv').config();
-const PORT = 3000;
+//require('dotenv').config();
+const PORT = 4000;
 
 //const PORT = parseInt(process.env.PORT);
 
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,46 @@
+const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator');
 
+const User = require('../models/User');
+
 const controller = {
+	register: (req, res) => {
+        return res.render('users/register');
+    },
+	processRegister: (req, res) => {
+		const resultValidation = validationResult(req);
+		
+		if (resultValidation.errors.length > 0) {
+			return res.render('users/register', {
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		}
+
+		let userInDB = User.findByField('email', req.body.email);
+
+		if (userInDB) {
+			return res.render('users/register', {
+				errors: {
+					email: {
+						msg: 'Este email ya está registrado'
+					}
+				},
+				oldData: req.body
+			});
+		}
+
+		let userToCreate = {
+			...req.body,
+			password: bcryptjs.hashSync(req.body.password, 10),
+			avatar: req.file.filename
+		}
+
+		let userCreated = User.create(userToCreate);
+		return res.redirect('/users/login');
+	},
+
+
 
     newProducts: (req, res) => {
 		return res.render('newProducts');
@@ -23,22 +63,7 @@ const controller = {
     signup: (req, res) => {
         res.render('users/signup')
     },
-
-	register: (req, res) => {
-        res.render('users/register')
-    },
-	processRegister: (req, res) => {
-		const resultValidation = validationResult(req);
-		
-		if (resultValidation.errors.length > 0) {
-			return res.render('register', {
-				errors: resultValidation.mapped(),
-				oldData: req.body
-			});
-		}
-		return res.send('Ok, las validaciones se pasaron y no tienes errores');
-	},
-    
+   
         
     login: (req, res) => {
       return res.render('users/login')
